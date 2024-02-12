e4575bb3641ea19fe3f499ff30943ef9acf1947a
cake-oh <108202245+cake-oh@users.noreply.github.com>
2024-02-12 13:10:11 -0800

    lab 3 stuffs

--- helloworld/js/scripts_lab3.js
+++ helloworld/js/scripts_lab3.js
@@ -12,15 +12,16 @@ L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.{ext
 }).addTo(map31); // add that tile layer to the map object
 
 // this fetches the local geojson data
-fetch('/data/usa-state-capitals.geojson')
+fetch('/data/map.geojson')
     .then(response => response.json())
     .then(data => {
         L.geoJSON(data, 
             {
             pointToLayer: function(feature, latlng) {
                 var capitalName = feature.properties.name;
-                var markerSize = capitalName.length*0.7; // Adjust multiplier as needed
                 var stateName = feature.properties.state;
+                var capitalPop = feature.properties.pop2024;
+                var markerSize = capitalPop*.000008; // Adjust multiplier as needed
                 return L.circleMarker(latlng, {
                     radius: markerSize,
                     fillColor: "#ff7800",
@@ -28,7 +29,7 @@ fetch('/data/usa-state-capitals.geojson')
                     weight: 1,
                     opacity: 1,
                     fillOpacity: 0.8
-                }).bindPopup("State: " + stateName + "<br>Capital: " + capitalName);
+                }).bindPopup(capitalName + ", " + stateName + "<br>Population: " + numberWithCommas(capitalPop));
             }
         })
         .addTo(map31);
@@ -36,8 +37,41 @@ fetch('/data/usa-state-capitals.geojson')
     .catch(error => console.error('Error: ', error));
 
 
-    
+
 //// choropleth data - state capital geojson ////
+var map32 = L.map('mappy2_lab3').setView([45, -105], 3); // leaflet; make a map object called map; add it to the DOM object where mappy is
+
+L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+}).addTo(map32); // add that tile layer to the map object
+
+fetch('/data/tl_2023_us_state.json')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data,
+            {
+                "color": "#ff7800",
+                "weight": 5,
+                "opacity": 0.65
+            }
+            ).addTo(map32);
+    })
+    .catch(error => console.error('Error: ', error));
 
 
+
+
+
+
+
+
+    
+//// establish functions ////
+/// number formatting
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
